3498dfca2170d19511ab0b5b12c49cae3c83477c
Aditya Pacharne <48561911+AdityaPacharne@users.noreply.github.com>
2025-02-12 00:27:04 +0530

    added VimBeGood

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -110,6 +110,11 @@ _G.packer_plugins = {
     path = "/Users/guts/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
+  ["vim-be-good"] = {
+    loaded = true,
+    path = "/Users/guts/.local/share/nvim/site/pack/packer/start/vim-be-good",
+    url = "https://github.com/ThePrimeagen/vim-be-good"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/Users/guts/.local/share/nvim/site/pack/packer/start/vim-fugitive",
